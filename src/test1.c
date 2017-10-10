4039190872cb834794d65a7d2174b835d1674c5f
Ryan Matthews <ryanmatthews@pitt.edu>
2017-10-10 15:03:30 -0400

    Make test1.c into blinky

--- src/test1.c
+++ src/test1.c
@@ -4,19 +4,35 @@
 #include <util/delay.h>
 #include <stdlib.h>
 
-int test = 7;
-
-ISR(PCINT2_vect)
-{
-    int x = 0;
-    ++x;
-}
+//
+//int test = 7;
+//
+//ISR(PCINT2_vect)
+//{
+//    int x = 0;
+//    ++x;
+//}
 
 int main()
 {
-    int x;
+    //int x;
 
-    for (size_t i = 0; i < 10; ++i) ++x;
+    //for (size_t i = 0; i < 10; ++i) ++x;
 
-    SREG &= 1<<7;
+    //SREG &= 1<<7;
+
+    // output
+    DDRD |= (1<<PD6);
+
+    for (;;) {
+        _delay_ms(3000);
+
+        // low
+        PORTD &= ~(1<<PD6);
+
+        _delay_ms(3000);
+
+        // high
+        PORTD |= 1<<PD6;
+    }
 }
