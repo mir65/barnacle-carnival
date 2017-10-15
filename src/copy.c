98a1792bfbfc44d09413d27b9fd5fd6a7533582f
Ryan Matthews <ryanmatthews@pitt.edu>
2017-10-14 22:10:38 -0400

    Copy does slightly more than blinky

---
+++ src/copy.c
@@ -0,0 +1,20 @@
+#include <stdbool.h>
+#include "dev/pin.h"
+#include "dev/interrupt.h"
+
+void copy()
+{
+    bool input = dev_pin_read(13);
+    dev_pin_write(22, input);
+}
+
+int main()
+{
+    dev_pin_input(13);
+    dev_pin_output(22);
+
+    dev_interrupt_init();
+    dev_interrupt_bind(13, copy);
+
+    for (;;);
+}
