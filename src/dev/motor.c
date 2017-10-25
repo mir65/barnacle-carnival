3cf4826837905be70e59657db81152610ae7c092
Ryan Matthews <ryanmatthews@pitt.edu>
2017-10-25 12:40:59 -0400

    Fixing motor voltage for reverse

--- src/dev/motor.c
+++ src/dev/motor.c
@@ -17,6 +17,8 @@ void dev_motor_voltage(struct dev_motor *object, int_fast16_t value)
 
     bool direction = value >= 0;
 
+    value = direction ? value : -value;
+
     dev_pin_write(object->direction1_pin,  direction);
     dev_pin_write(object->direction2_pin, !direction);
     dev_timer_duty_cycle(object->voltage_pin, value);
