e12136c55cb23138fbb21ea57683c5b7317eda0a
Ryan Matthews <ryanmatthews@pitt.edu>
2017-11-01 23:40:43 -0400

    Potentiometer test

---
+++ test/pot.c
@@ -0,0 +1,37 @@
+#define F_CPU 16000000
+
+#include <stdio.h>
+#include <dev/delay.h>
+#include <dev/pot.h>
+#include <dev/serial.h>
+
+int main()
+{
+    dev_serial_module_init();
+    dev_pot_module_init();
+
+    struct dev_pot pot = {
+        .pin = 0
+    };
+
+    dev_pot_init(&pot);
+
+    int_fast16_t last_count = 0;
+
+    for (;;) {
+        dev_delay_ms(100);
+
+        char line[81];
+
+        int_fast16_t count = dev_pot_position(&pot);
+
+        if (count == last_count)
+            continue;
+
+        last_count = count;
+
+        sprintf(line, "%4d\n", count);
+
+        dev_serial_tx(line);
+    }
+}
