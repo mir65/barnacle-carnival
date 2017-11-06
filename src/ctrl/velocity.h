2da946513d7bb9aa68859027392b489a62185f20
mir65 <33430887+mir65@users.noreply.github.com>
2017-11-06 14:04:25 -0500

    Update velocity.h

--- src/ctrl/velocity.h
+++ src/ctrl/velocity.h
@@ -1,3 +1,39 @@
 /* Initializes the velocity controller. */
 
 void ctrl_velocity_init();
+{
+ 
+double sp,pv,reset,o0
+    
+    
+error:=sp-pv;
+if reset then
+old:=0.0;
+
+else
+old:=old;
+outKP:=kp*error;
+outKD:=kd*(error-i0_old)/sample_time;
+i0_old:=error;
+end_if;
+ 
+if error*sample_time*ki+old+outKP+outKD>limit_upper then
+o0:=limit_upper;
+old:=limit_upper;
+
+else
+if error*sample_time*ki+old+outKP+outKD<limit_lower then
+o0:=limit_lower;
+old:=limit_lower;
+
+else
+o0 := error*sample_time*ki+old+outKP+outKD;
+old:=o0-outKP-outKD;
+end_if;
+end_if;
+    
+  
+  
+  
+  
+}
